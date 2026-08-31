dfe4e647c9561f031f50959b084a830be40ba3f3
pabloDev342 <juan_castro82222@elpoli.edu.co>
2026-08-31 13:05:18 -0500

    Add palindrome function and format code

--- recursividad.py
+++ recursividad.py
@@ -4,3 +4,15 @@ def factorial(n):
     return n * factorial(n - 1)
 
 print(factorial(5))
+
+import time
+
+def palindromo(s):
+        if len(s) <=1:
+            return True
+
+        if s[0] != s[-1]:
+              return False
+        return palindromo(s[1:-1])
+print (palindromo("reconocer"))
+ 
